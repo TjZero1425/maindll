70251d87315683f35e66c495996030b60d203407
Myperior <95068783+TjZero1425@users.noreply.github.com>
2025-01-19 10:26:34 +0100

    Create string.find.lua

---
+++ string.find.lua
@@ -0,0 +1,17 @@
+-- fix implenmenatation to string.find
+
+setreadonly(string, false)
+local original_find = string.find
+function string.find(subject, pattern, ...)
+    if type(subject) ~= "string" then
+        subject = tostring(subject)
+    end
+    if type(pattern) ~= "string" then
+        error("Invalid argument #2 to 'find' (string expected, got " .. type(pattern) .. ")", 2)
+    end
+    local success, result1, result2 = pcall(original_find, subject, pattern, ...)
+    if not success then
+        error("Error in string.find: " .. result1, 2)
+    end
+    return result1, result2
+end
