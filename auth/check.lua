ab61b1c8247369cf76ae2e63040f12f5b2a6552f
Myperior <95068783+TjZero1425@users.noreply.github.com>
2025-01-22 17:25:17 +0100

    Update check.lua

--- auth/check.lua
+++ auth/check.lua
@@ -1,6 +1,6 @@
 local data = getexecutorkeyforlornultimate()
 
-if (data == "SUPERCOOL123")
+if (data == "SUPERCOOL123") then
      return
 else
      error("You cannot execute without a valid license.")
